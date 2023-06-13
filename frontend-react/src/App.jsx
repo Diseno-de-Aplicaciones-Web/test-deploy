2a2c15031ef2097671f28fe5b5404bc957810a36
DanBaDo <bytes4life@proton.me>
2023-06-13 16:52:33 +0200

    Provides frontend test

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [datos, setDatos] = useState([])
+
+  async function cargarDatosBackend() {
+    const respuesta = await fetch("http://localhost:8000/api/datos/")
+    if (respuesta.ok) setDatos(await respuesta.json())
+  }
+
+  useEffect(
+    () => {cargarDatosBackend()},
+    []
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Frontend React</h1>
+      <p>Hostname: {window.location.hostname}</p>
+      <p>Host: {window.location.host}</p>
+      <h2>Datos do backend:</h2>
+      {
+        datos.length ?
+          datos.map( elemento => <p>{elemento}</p> )
+          :
+          <p>Sin datos</p>
+      }
+    </main>
   )
+
 }
 
 export default App
