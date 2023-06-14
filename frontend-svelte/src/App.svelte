d1c191461fcd7cd3040602cb7ac32da5170c45d6
DanBaDo <bytes4life@proton.me>
2023-06-14 09:16:03 +0200

    Provides Svelte frontend

---
+++ frontend-svelte/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+
+  let datos = []
+
+  async function cargarDatosBackend() {
+    const respuesta = await fetch("http://localhost:8000/api/datos/")
+    if (respuesta.ok) datos = await respuesta.json()
+  }
+
+  cargarDatosBackend()
+
+</script>
+<main>
+  <h1>Frontend React</h1>
+  <p>Hostname: {window.location.hostname}</p>
+  <p>Host: {window.location.host}</p>
+  <h2>Datos do backend:</h2>
+  {#if datos.length }
+    {#each datos as elemento }
+    <p>{elemento.frase}</p>
+    {/each}
+  {:else}
+  <p>Sin datos</p>
+  {/if}
+
+</main>
