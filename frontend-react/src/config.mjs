42a3d11a59ce5f214c210252145c8d052ebb7aa0
DanBaDo <bytes4life@proton.me>
2023-06-14 19:50:03 +0200

    Includes 127.0.0.1 when testing if frontend is running in production

--- frontend-react/src/config.mjs
+++ frontend-react/src/config.mjs
@@ -1,4 +1,4 @@
-const  BACKEND_URL = window.location.hostname === "localhost"
+const BACKEND_URL = ["localhost", "127.0.0.1"].includes(window.location.hostname)
     ? "http://localhost:8000"
     : "https://backend-xwut.onrender.com"
 
