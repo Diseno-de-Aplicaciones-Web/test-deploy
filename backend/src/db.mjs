799be266af83d9cf7a1c5b3a73d43fb73b76c1ae
DanBaDo <bytes4life@proton.me>
2023-06-14 10:23:23 +0200

    Configura la base de datos en base a la varaible de entorno

--- backend/src/db.mjs
+++ backend/src/db.mjs
@@ -1,6 +1,6 @@
 import { Sequelize, DataTypes } from "sequelize"
 
-const db = new Sequelize('sqlite:baseDeDatos.sqlite')
+const db = new Sequelize( process.env.DB_URL ?? 'sqlite:baseDeDatos.sqlite')
 
 const Dato = db.define("Dato",{
     frase: {
